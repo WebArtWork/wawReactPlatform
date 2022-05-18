087ab86729148cee4569ffd82d2008c5d23818e5
ArturHoncharuk <dotwork.play@gmail.com>
2022-05-18 13:33:07 +0300

    master

---
+++ client/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '../styles/globals.scss'
+import '../styles/Login.scss'
+
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,47 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import UserService from '../services/user.service'
+import React, { Component } from 'react'
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
-const Home: NextPage = () => {
-	const us = new UserService();
-	const us1 = new UserService();
-	console.log(us.rand, us1.rand);
-	return (
-		<h1>Hello World</h1>
-	)
+
+// const Home: NextPage = () => {
+// 	return (
+
+// 	)
+// }
+
+export default class Home extends Component {
+	constructor() {
+		super()
+		this.state = {
+			showPassword: false,
+		}
+	}
+	render() {
+		return (
+			<div className="auth-wrapper">
+				<div className="auth">
+					<div className="auth__title">Sign In / Sign Up</div>
+					<form className="auth__forms">
+						<div className="w-forms">
+							<span className="w-forms__title">Email:</span>
+							<input className="w-forms__input" type="text" placeholder="Email" name="email" />
+
+						</div>
+						<div className="w-forms">
+							<span className="w-forms__title">Password:</span>
+							<input type={this.state.showPassword ? 'text' : 'password'} className="w-forms__input" placeholder="Password" name="password" />
+							<span onClick={() => this.setState({ showPassword: !this.state.showPassword })}>
+								{this.state.showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+							</span>
+						</div>
+						<div className="auth__btn">
+							<button className="w-btn _primary" type="submit">Let's go</button>
+						</div>
+					</form>
+				</div>
+			</div >
+		)
+	}
 }
 
-export default Home
