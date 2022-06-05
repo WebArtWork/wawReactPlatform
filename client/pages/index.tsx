9972b7b2f0e9f2c034ce50973fd5d6e1f5203686
ArturHoncharuk <dotwork.play@gmail.com>
2022-06-05 16:37:56 +0300

    index-fix

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -108,16 +108,6 @@ const Login: NextPage = () => {
     }
 
     return (
-        <Head>
-            <link
-                  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
-            <link rel="stylesheet"
-                  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
-            <link rel="stylesheet"
-                  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
-            <link rel="stylesheet"
-                  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"/>
-        </Head>
     <div className="auth-wrapper">
         <div className="auth">
             <div className="auth__title">Sign In / Sign Up</div>
