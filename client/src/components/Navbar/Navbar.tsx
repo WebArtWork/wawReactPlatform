a67fa495e16faaf104d3f8bcbb0c383d9bd4090a
ArturHoncharuk <dotwork.play@gmail.com>
2022-06-23 22:31:13 +0300

    hamburger-menu

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import Link from 'next/link'
-import {userRoutes, adminRoutes} from "../../app/routes";
-import {useAppSelector} from "Hooks/useRedux";
-import {IUser} from "Types/IUser";
+import { userRoutes, adminRoutes } from "../../app/routes";
+import { useAppSelector } from "Hooks/useRedux";
+import { IUser } from "Types/IUser";
 
 const Navbar = () => {
-    // const router = useRouter();
     const [mounted, setMounted] = useState(false);
+    const [show, setShow] = useState(false)
     useEffect(() => {
         setMounted(true)
     }, [])
     const user = useAppSelector<IUser>((state) => state.user.user);
+    const handleClick = () => {
+        setShow(!show)
+    }
     return (
         mounted &&
         <nav className="navbar">
@@ -24,6 +27,29 @@ const Navbar = () => {
                     <a className="navbar-item">{route.name}</a>
                 </Link>
             )) : ''}
+
+            <div className="hamburger">
+                <div className="hamburger__icon" onClick={handleClick}>
+                    <div className={!show ? 'hamburger__icon-item' : 'hamburger__icon-item unactive'}></div>
+                    <div className={!show ? 'hamburger__icon-item' : 'hamburger__icon-item unactive'}></div>
+                    <div className={!show ? 'hamburger__icon-item' : 'hamburger__icon-item unactive'}></div>
+                </div>
+
+                <div className={show ? 'hamburger__list' : 'hamburger__hide'} onClick={() => setShow(false)}>
+                    <div className="hamburger__wrapper">
+                        {userRoutes.map(route => (
+                            <Link key={route.link} className="navbar-link" href={route.link}>
+                                <a className="hamburger-item">{route.name}</a>
+                            </Link>
+                        ))}
+                        {user.is.admin ? adminRoutes.map((route) => (
+                            <Link key={route.link} className="navbar-link" href={route.link}>
+                                <a className="hamburger-item">{route.name}</a>
+                            </Link>
+                        )) : ''}
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
